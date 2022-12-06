09dd027381e7889e37ba5aa7b99c04029337a97a
Aryan Chandramania <arashaurchandramania@gmail.com>
2022-12-06 10:57:40 +0530

    added day 6

---
+++ Day 6/day6_2022.py
@@ -0,0 +1,11 @@
+def solve(n):
+    for i in range(0,len(signal)):
+        packet = signal[i:i+n]
+        if len(set(packet)) == n:
+            return i+n
+
+with open('input.txt','r') as f:
+    signal = f.read().strip()
+
+    print(f"First result: {solve(4)}")
+    print(f"Second result: {solve(14)}")
