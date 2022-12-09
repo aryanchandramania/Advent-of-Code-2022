e916824e013037498fbb1c226d60bbdfacaf092c
Aryan Chandramania <arashaurchandramania@gmail.com>
2022-12-09 19:31:13 +0530

    added day 9 (inc. readability)

--- Day 9/day9_2022.py
+++ Day 9/day9_2022.py
@@ -1,23 +1,23 @@
-"""The one with the rope bridge/The one with the snake"""
+"""The one with the rope bridge/The one with the snake  """
 
-d = {"U": +1j, "D": -1j, "L": -1, "R": +1}
+directions = {"U": +1j, "D": -1j, "L": -1, "R": +1}
 rope = [0 + 0j] * 10
 seen_1, seen_9 = {rope[1]}, {rope[-1]}
 
 for line in open("input.txt").read().splitlines():
-    dir, steps = line.split()
+    d, steps = line.split()
 
     for _ in range(int(steps)):
         for i in range(len(rope)):
             if i == 0:
-                rope[i] += d[dir]
+                rope[i] += directions[d]
                 continue
 
             if abs(diff := rope[i - 1] - rope[i]) >= 2:
-                if (a := abs(diff.real)) > 0:
-                    rope[i] += complex(diff.real / a, 0)
-                if (a := abs(diff.imag)) > 0:
-                    rope[i] += complex(0, diff.imag / a)
+                if (movement := abs(diff.real)) > 0:
+                    rope[i] += complex(diff.real / movement, 0)
+                if (movement := abs(diff.imag)) > 0:
+                    rope[i] += complex(0, diff.imag / movement)
 
         seen_1.add(rope[1])
         seen_9.add(rope[-1])
